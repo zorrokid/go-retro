5e35c0da8630823f3ae256cbc72aae7c17717c98
Mikko Keinänen <mikko.keinanen@gmail.com>
2022-01-07 12:54:44 +0200

    Add and list titles

--- ui/go-retro-ui.go
+++ ui/go-retro-ui.go
@@ -1,22 +1,107 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"github.com/zorrokid/go-retro/database"
+	"github.com/zorrokid/go-retro/database/model"
+	"github.com/zorrokid/go-retro/ui/services"
 )
 
+var db *database.Database
+var titleService *services.TitleService
+var data []model.Title
+var list *widget.List
+
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello")
+	db = database.NewDatabase()
+	db.InitDB()
+	titleService = services.NewTitleService(db)
+	data = titleService.GetTitles()
 
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
+	app := app.New()
+	mainWindow := app.NewWindow("Go-Retro!")
+	mainWindow.SetMainMenu(makeMenu(app, mainWindow))
+	mainWindow.SetContent(makeListTab(mainWindow))
+	mainWindow.ShowAndRun()
+}
+
+func makeMenu(a fyne.App, w fyne.Window) *fyne.MainMenu {
+	newItem := fyne.NewMenuItem("New", nil)
+	newItem.ChildMenu = fyne.NewMenu("",
+		fyne.NewMenuItem("Title", func() {
+			openAddTitleDialog(w)
 		}),
-	))
+	)
 
-	w.ShowAndRun()
+	helpMenu := fyne.NewMenu("Help",
+		fyne.NewMenuItem("Info", func() {
+			fmt.Println("Add info")
+		}),
+	)
+
+	file := fyne.NewMenu("File", newItem)
+	return fyne.NewMainMenu(
+		file,
+		helpMenu,
+	)
+}
+
+func openAddTitleDialog(win fyne.Window) {
+	titlename := widget.NewEntry()
+	items := []*widget.FormItem{
+		widget.NewFormItem("Title name", titlename),
+	}
+
+	dialog.ShowForm("Add title", "Add", "Cancel", items, func(b bool) {
+		if !b {
+			return
+		}
+		log.Println("Add title", titlename.Text)
+		titleService.AddTitle(titlename.Text)
+		refreshData()
+	}, win)
+}
+
+func refreshData() {
+	data = titleService.GetTitles()
+	list.Refresh()
+}
+
+func makeListTab(_ fyne.Window) fyne.CanvasObject {
+
+	icon := widget.NewIcon(nil)
+	label := widget.NewLabel("Select An Item From The List")
+	hbox := container.NewHBox(icon, label)
+
+	list = widget.NewList(
+		func() int {
+			return len(data)
+		},
+		func() fyne.CanvasObject {
+			return container.NewHBox(widget.NewIcon(theme.DocumentIcon()), widget.NewLabel("Template Object"))
+		},
+		func(id widget.ListItemID, item fyne.CanvasObject) {
+			item.(*fyne.Container).Objects[1].(*widget.Label).SetText(data[id].Name)
+		},
+	)
+	list.OnSelected = func(id widget.ListItemID) {
+		label.SetText(data[id].Name)
+		icon.SetResource(theme.DocumentIcon())
+	}
+	list.OnUnselected = func(id widget.ListItemID) {
+		label.SetText("Select An Item From The List")
+		icon.SetResource(nil)
+	}
+
+	list.Select(125)
+
+	return container.NewHSplit(list, container.NewCenter(hbox))
 }
--- ui/go.mod
+++ ui/go.mod
@@ -2,7 +2,11 @@ module github.com/zorrokid/go-retro/ui
 
 go 1.17
 
-require fyne.io/fyne/v2 v2.1.2
+require (
+	fyne.io/fyne/v2 v2.1.2
+	github.com/zorrokid/go-retro/database v0.0.0-20220102220145-c1a27ea4f28f
+	github.com/zorrokid/go-retro/repository v0.0.0-20220102220145-c1a27ea4f28f
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -12,14 +16,21 @@ require (
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20211024062804-40e447a793be // indirect
 	github.com/godbus/dbus/v5 v5.0.4 // indirect
 	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/mattn/go-sqlite3 v1.14.10 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/srwiley/oksvg v0.0.0-20200311192757-870daf9aa564 // indirect
 	github.com/srwiley/rasterx v0.0.0-20200120212402-85cb7272f5e9 // indirect
-	github.com/stretchr/testify v1.5.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/yuin/goldmark v1.3.8 // indirect
 	golang.org/x/image v0.0.0-20200430140353-33d19683fad8 // indirect
 	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/text v0.3.3 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	gorm.io/driver/sqlite v1.2.6 // indirect
+	gorm.io/gorm v1.22.4 // indirect
 )
+
+replace github.com/zorrokid/go-retro/repository => ../repository
