23fda6101c40202061ef9b9341e3cab5819979a7
Mikko Keinänen <mikko.keinanen@gmail.com>
2022-01-08 18:44:41 +0200

    List releases

--- ui/selected-title.go
+++ ui/selected-title.go
@@ -5,7 +5,7 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/zorrokid/go-retro/database/model"
 	"github.com/zorrokid/go-retro/ui/services"
@@ -32,23 +32,20 @@ func NewSelectedTitle(title *model.Title, window *fyne.Window, titleService *ser
 func (selected *SelectedTitle) makeSelectedTitleContent() fyne.CanvasObject {
 	icon := widget.NewIcon(nil)
 
-	data := binding.BindStringList(
-		&[]string{"Item 1", "Item 2", "Item 3"},
-	)
-
 	add := widget.NewButton("Add release", func() {
-		//val := fmt.Sprintf("Item %d", data.Length()+1)
-		//data.Append(val)
 		selected.openReleaseDialog()
 	})
 	hbox := container.NewHBox(icon, selected.titleLabel, add)
 
-	itemList := widget.NewListWithData(data,
-		func() fyne.CanvasObject {
-			return widget.NewLabel("template")
+	itemList := widget.NewList(
+		func() int {
+			return len(selected.title.Releases)
 		},
-		func(i binding.DataItem, o fyne.CanvasObject) {
-			o.(*widget.Label).Bind(i.(binding.String))
+		func() fyne.CanvasObject {
+			return container.NewHBox(widget.NewIcon(theme.DocumentIcon()), widget.NewLabel("Template Object"))
+		},
+		func(id widget.ListItemID, item fyne.CanvasObject) {
+			item.(*fyne.Container).Objects[1].(*widget.Label).SetText(selected.title.Releases[id].Edition)
 		})
 
 	return container.NewBorder(hbox, nil, nil, nil, itemList)
